bb96fb8d7032c0442f26d90124db066573908acc
Dovganyuk <asdovganyuk@gmail.com>
2021-09-21 10:15:12 +0300

    review ready version 4

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -40,18 +40,18 @@ class TaskPagesTests(TestCase):
     def test_pages_uses_correct_template(self):
         """URL-адрес использует соответствующий шаблон."""
         templates_pages_names = {
-            'posts/index.html': reverse('posts:index'),
-            'posts/group_list.html': reverse('posts:group',
-                                             kwargs={'slug': 'test-slug'}),
-            'posts/profile.html': reverse('posts:profile',
-                                          kwargs={'username': 'author'}),
-            'posts/post_detail.html': (reverse('posts:post_detail',
-                                       kwargs={'post_id': '0'})),
+            reverse('posts:index'): 'posts/index.html',
+            reverse('posts:group',
+                    kwargs={'slug': 'test-slug'}): 'posts/group_list.html',
+            reverse('posts:profile',
+                    kwargs={'username': 'author'}): 'posts/profile.html',
+            reverse('posts:post_detail',
+                    kwargs={'post_id': '0'}): 'posts/post_detail.html',
             reverse('posts:post_edit',
                     kwargs={'post_id': '0'}): 'posts/create_post.html',
             reverse('posts:post_create'): 'posts/create_post.html',
         }
-        for template, reverse_name in templates_pages_names.items():
+        for reverse_name, template in templates_pages_names.items():
             with self.subTest(reverse_name=reverse_name):
                 response = self.authorized_client.get(reverse_name)
                 self.assertTemplateUsed(response, template)
